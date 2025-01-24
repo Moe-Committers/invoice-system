e49fdaaa6972b38abf5307d3dcd9ea7a5f78c616
Haichhan <ppfreea1@gmail.com>
2025-01-25 03:14:57 +0700

    fixs

--- Models/RefreshTokens.cs
+++ Models/RefreshTokens.cs
@@ -1,5 +1,16 @@
 namespace invoice_system.Models;
 
 public class RefreshTokens : BaseEntity{
-    
+    public long UserId {get; set;}
+    public string Token {get; set;}
+    public DateTime ExpiredAt {get; set;}
+    public string CreatedByIp {get; set;}
+    public DateTime? RevokedAt {get; set;}
+    public string? ReasonRevoked {get; set;}
+    public string? ReplaceByToken {get; set;}
+    public string? RevokedByIp {get; set;}
+    public bool isExpired => DateTime.UtcNow >= ExpiredAt;
+    public bool isRevoked => RevokedAt != null;
+    public bool isActive => !isExpired && !isRevoked;
+    public virtual Users user {get; set;}
 }
